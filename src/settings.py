7306d52107b930312395e51c1418613a9a315ef0
Ahme460 <ahmeoon1234@gmail.com>
2025-02-05 00:25:47 +0200

    env

--- src/settings.py
+++ src/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-g8v@+6i@2cz4j-ce+knphp_y4d!87$v8gq-i3ip)egnn+-#(t(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["ahmedmahmoud10.pythonanywhere.com"]
 
 
 # Application definition
