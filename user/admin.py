a69074e7dc099e6b84cf73d04e5a099489a5b06e
Ahme460 <ahmeoon1234@gmail.com>
2025-02-06 02:59:55 +0200

    device

--- user/admin.py
+++ user/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
-from .models import Account
+from .models import *
 
 
 @admin.register(Account)
+
 class AccountAdmin(admin.ModelAdmin):
     list_display = ['email', 'username','date_joined']
     list_filter=['username','country']
     readonly_fields = ('date_joined', 'last_login')  
     ordering = ['-date_joined']
+    
+admin.site.register(UserDevice)
