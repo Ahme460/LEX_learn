8fda7b3591a59a986d1b4013c89eea2bbf6ac021
Ahme460 <ahmeoon1234@gmail.com>
2025-02-10 10:31:33 +0200

    up

--- src/settings.py
+++ src/settings.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60*60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
